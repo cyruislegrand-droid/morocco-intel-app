d7c7140e1d9987d89ada3812a1c47d2e925274d8
cyruislegrand-droid <cyruislegrand@gmail.com>
2026-05-01 21:53:01 +0100

    Update analysis.py

--- engine/analysis.py
+++ engine/analysis.py
@@ -1,10 +1,40 @@
+import pandas as pd
 import networkx as nx
+from textblob import TextBlob
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-def build_actor_network(df):
+# Initialize VADER
+analyzer = SentimentIntensityAnalyzer()
+
+def compute_sentiment(text):
+    """
+    Calculates a sentiment score for the given text.
+    """
+    if not text or pd.isna(text):
+        return 0.0
+    try:
+        # Hybrid score
+        blob_score = TextBlob(str(text)).sentiment.polarity
+        vader_score = analyzer.polarity_scores(str(text))['compound']
+        return (blob_score + vader_score) / 2
+    except Exception:
+        return 0.0
+
+def build_actor_network(news_df):
+    """
+    Creates a structural network of actors.
+    """
     G = nx.Graph()
-    for _, row in df.iterrows():
-        actors = row['actors']
-        for i in range(len(actors)):
-            for j in range(i + 1, len(actors)):
-                G.add_edge(actors[i], actors[j], weight=1)
+    if news_df.empty:
+        return G
+        
+    for _, row in news_df.iterrows():
+        actors = row.get('actors', [])
+        if isinstance(actors, list):
+            for i in range(len(actors)):
+                for j in range(i + 1, len(actors)):
+                    if G.has_edge(actors[i], actors[j]):
+                        G[actors[i]][actors[j]]['weight'] += 1
+                    else:
+                        G.add_edge(actors[i], actors[j], weight=1)
     return G
